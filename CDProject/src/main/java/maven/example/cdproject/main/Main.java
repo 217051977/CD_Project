ae5f6c618ad0ea2b8d956b9e6716a8475fdb6090
Bruno Leal <a21705197@alunos.ulht.pt>
2021-01-05 20:53:14 +0000

    Add project

---
+++ CDProject/src/main/java/maven/example/cdproject/main/Main.java
@@ -0,0 +1,13 @@
+package maven.example.cdproject.main;
+
+//import maven.example.cdproject.db.DB;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		//DB db = new DB();
+		
+	}
+
+}
